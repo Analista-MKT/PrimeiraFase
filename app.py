fb4464ed334ee442c1435ec5b173a940ec2df9a7
Analista-MKT <pereirat@desangosse.com.br>
2025-04-24 14:42:53 -0300

    Uso da função criada

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 import pandas as pd
+from functions import import_file
+
+repositorio = 'C:\Users\pereirat\OneDrive - De Sangosse Agroquímica Ltda\Documentos\4.0 - PROGRAMAÇÃO E BANCO DE DADOS'
+arquivo = 'faturamento'
+planilha = 'DADOS'
+df_vendas = import_file(repositorio, arquivo, planilha)
 
 
-df = r'C:\Users\pereirat\OneDrive - De Sangosse Agroquímica Ltda\Documentos\1.0 - INDICADORES\1.6 - Banco de Dados\1.6.7 - DADOS QLIK SENSE\1.6.7.1 - VENDAS\Vendas 2024 até 25-11.xlsx'
+print(df_vendas.head())
 
-Vendas = pd.read_excel(df, sheet_name='DADOS')
+faturamento_total = df_vendas['Valor Real'].sum()
+Volume_total = df_vendas['Quantidade'].sum()
+
+
+print(format(faturamento_total, '.2f'))
+
 
 
 #print(Vendas.head())
